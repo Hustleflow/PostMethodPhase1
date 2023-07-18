2af319f0ee0e5a6b26ad8fcf6bdfe52ac36d1b47
Hustleflow <banados.jg@gmail.com>
2023-07-17 23:49:47 -0500

    basicsOfUsingPostMethodAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-function App() {
+import { useState } from 'react';
+
+
+
+
+const App = () => {
+
+const [task, setTask] = useState("");
+
+const [taskList, setTaskList] = useState([]);
+
+
+const getApi = async () =>{
+  const res = await axios.get('http://localhost:3007');
+    console.log(res.data);
+}
+  
+getApi();
+
+
+const animal = "bullshit";
+
+const sendInput = async () => {
+  await axios.post('http://localhost:3007/tasks', {animal});
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container p-4">
+      <div>
+        <h1>weird div</h1>
+        <img src="https://picsum.photos/200/300" alt="random"/>
+        <input type="text" placeholder="type something" onChange={(event) => setTask(event.target.value)}/>
+        <button className="btn btn-primary" onClick={() => sendInput() }>Click me</button>
+      </div>
+
     </div>
   );
-}
+};
 
 export default App;
