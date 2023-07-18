305a825499f20297053d5aed9986c23a1ddd8bc6
Hustleflow <banados.jg@gmail.com>
2023-07-18 01:55:35 -0500

    addedTaskList

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 
@@ -14,13 +15,16 @@ const [task, setTask] = useState("");
 
 const [taskList, setTaskList] = useState([]);
 
-
 const getApi = async () =>{
-  const res = await axios.get('http://localhost:3007');
-    console.log(res.data);
+  const res = await axios.get('http://localhost:3007/tasks');
+    // console.log(res.data);
+      setTaskList(res.data);
 }
-  
-getApi();
+
+useEffect ( () => {
+    getApi()
+}, []);
+
 
 
 const animal = "bullshit";
@@ -34,7 +38,17 @@ const sendInput = async () => {
       <div>
         <h1>weird div</h1>
         <img src="https://picsum.photos/200/300" alt="random"/>
-        <input type="text" placeholder="type something" onChange={(event) => setTask(event.target.value)}/>
+
+        <h2>Task List</h2>
+        <ul>
+          {taskList.map((task) => (
+
+            <li>{task.task}</li>
+
+          ))}
+        </ul>
+
+        <input type="text" placeholder="type `something" onChange={(event) => setTask(event.target.value)}/>
         <button className="btn btn-primary" onClick={() => sendInput() }>Click me</button>
       </div>
 
